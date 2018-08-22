d14a0a46ccb48335fd34e63140eb7e1358616d57
jrichard <jrichard@synopsys.com>
2018-08-22 14:27:04 -0400

    Updating jacoco stage

--- vars/jacocoStage.groovy
+++ vars/jacocoStage.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 
-def call(String stageName = 'Record JaCoCo coverage') {
+def call(String stageName = 'Record JaCoCo coverage', Closure body) {
 
     stage(stageName) {
         step([$class: 'JacocoPublisher'])
