e56c9cfe039346e23159be1708944a7389d54bbc
jrichard <jrichard@synopsys.com>
2018-08-23 17:03:59 -0400

    Removing windows support

--- vars/mavenStage.groovy
+++ vars/mavenStage.groovy
@@ -6,27 +6,11 @@ def call(String stageName = 'Maven Build', Closure body) {
     body.delegate = config
     body()
 
-    def mavenExe = config.exe
     def mavenToolName = config.get('toolName', 'maven-3')
     def mavenBuildCommand = config.get('buildCommand', '-U clean package deploy')
 
     def mvnHome = tool "${mavenToolName}"
-
-    if (null == mavenExe || mavenExe.trim().length() == 0) {
-        File mavenHome = new File("${mvnHome}")
-        File mavenBin = new File(mavenHome, 'bin')
-        File mavenFile = mavenBin.listFiles().find {
-            it.getName().contains('mvn')
-        }
-        mavenExe = mavenFile.getAbsolutePath()
-    }
-
     stage(stageName) {
-        if (isUnix()) {
-            sh "${mavenExe} ${mavenBuildCommand}"
-        } else {
-
-            "cmd.exe /C \"${mavenExe} ${mavenBuildCommand}\"".execute().waitFor()
-        }
+        sh "${mvnHome}/bin/mvn ${mavenBuildCommand}"
     }
 }
--- vars/setupStage.groovy
+++ vars/setupStage.groovy
@@ -2,21 +2,7 @@
 
 def call(String stageName = 'Setup', Closure body) {
     stage(stageName) {
-        if (isUnix()) {
-            sh 'rm -rf *'
-        } else {
-            def currentDirectoryPath = pwd()
-            File currentDirectory = new File(currentDirectoryPath)
-            def files = currentDirectory.listFiles()
-            if (null != files && !files.isEmpty()) {
-                def delete = ''
-                files.each {
-                    delete = it.getAbsolutePath() + " " + delete
-                }
-                bat "echo ${delete}"
-                bat "rmdir /s /q ${delete}"
-            }
-        }
+        sh 'rm -rf *'
         body()
     }
 }
