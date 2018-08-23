2201a768f077e6811ef51a8355fd8c28edcc9cd1
jrichard <jrichard@synopsys.com>
2018-08-23 16:47:45 -0400

    debugging the windows maven

--- vars/mavenStage.groovy
+++ vars/mavenStage.groovy
@@ -14,9 +14,13 @@ def call(String stageName = 'Maven Build', Closure body) {
         if (isUnix()) {
             sh "${mvnHome}/bin/mvn ${mavenBuildCommand}"
         } else {
+            println "${mvnHome}"
+            println "${mavenToolName}"
             File directory = new File("${mvnHome}")
             directory = new File(directory, 'bin')
+            println directory.getAbsolutePath()
             def files = directory.listFiles()
+            println files.size()
             if (null != files && !files.isEmpty()) {
                 files.each {
                     println it.getAbsolutePath()
