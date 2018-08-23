9a470d384882c2caa2a4b84a1cf526e86312ac0b
jrichard <jrichard@synopsys.com>
2018-08-23 13:22:05 -0400

    Updating email wrapper

--- vars/emailWrapper.groovy
+++ vars/emailWrapper.groovy
@@ -14,7 +14,7 @@ def call(String recipientList = 'justiceleague@synopsys.com', Closure body) {
         if (currentBuild.result == "FAILURE") {
             println "Sending out Build Failure email: ${SUBJECT}"
             emailext body: CONTENT, subject: SUBJECT, to: TO
-        } else if (currentBuild.getPreviousBuild().result != "SUCCESS" && currentBuild.resultIsBetterOrEqualTo(currentBuild.getPreviousBuild().result)) {
+        } else if (null != currentBuild.getPreviousBuild() && currentBuild.getPreviousBuild().result != "SUCCESS" && currentBuild.resultIsBetterOrEqualTo(currentBuild.getPreviousBuild().result)) {
             SUBJECT = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - Fixed!"
             CONTENT = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - Fixed!\nCheck console output at ${env.BUILD_URL} to view the results."
             println "Sending out Build Fixed email: ${SUBJECT}"
