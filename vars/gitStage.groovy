33358c45bbc8760bc823d2d8af069d61abbe5e7f
jrichard <jrichard@synopsys.com>
2018-08-23 10:18:49 -0400

    Updating the branch handling in the gitstage

--- vars/gitStage.groovy
+++ vars/gitStage.groovy
@@ -6,6 +6,11 @@ def call(String stageName = 'Git', Closure body) {
     body.delegate = config
     body()
 
+    def branch = config.get('branch', "${BRANCH}")
+    if (null == branch || branch.trim().length() == 0) {
+        branch = 'master'
+    }
+
     def gitTool = config.get('git', 'Default')
     def changelog = config.get('changelog', false)
     def poll = config.get('poll', false)
@@ -13,7 +18,7 @@ def call(String stageName = 'Git', Closure body) {
 
     stage(stageName) {
         checkout changelog: changelog, poll: poll,
-                scm: [$class    : 'GitSCM', branches: [[name: config.branch]], doGenerateSubmoduleConfigurations: false,
+                scm: [$class    : 'GitSCM', branches: [[name: branch]], doGenerateSubmoduleConfigurations: false,
                       extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: relativeTargetDir]], gitTool: gitTool, submoduleCfg: [], userRemoteConfigs: [[url: config.url]]]
 
     }
