ed4b6e2cef86527c6ab7d7e083ee09ca072c3209
jrichard <jrichard@synopsys.com>
2018-08-24 11:52:23 -0400

    Refactoring code

--- src/com/synopsys/integration/ProjectUtils.groovy
+++ src/com/synopsys/integration/ProjectUtils.groovy
@@ -1,82 +1,77 @@
 package com.synopsys.integration;
 
-public String getMavenProjectVersion(String exe){
-    def mavenVersion = ''
-    try {
-        def mavenExe = 'mvn'
-        if(exe) {
-            mavenExe = exe
-        }
-        def mavenProcess = "${mavenExe} help:evaluate -Dexpression=project.version | grep -v '\\['".execute()
-        mavenProcess.waitFor()
-        mavenVersion = mavenProcess.getText()
-    } catch (Exception e) {
-        println "Failed to run the mvn command to get the Project version ${e.getMessage()}"
-    }
-    if (!mavenVersion){
-         def fileText =  new File('pom.xml').text
-         def project = new XmlSlurper().parseText(fileText)
-         mavenVersion = project.version.text()
-    }
 
-    return mavenVersion
+public String getProjectVersion(String tool, String exe){
+    if(tool.equals('maven')){
+        def version = getMavenProjectVersionProcess(exe)
+        if (!version){
+            version = getMavenProjectVersionParse()
+        }
+        println version
+        return version
+    } else {
+        def version = getGradleProjectVersionProcess(exe)
+        if (!version){
+            version = getGradleProjectVersionParse()
+        }
+        println version
+        return version
+    }
+}
+
+public String getProjectVersion(String tool){
+    if(tool.equals('maven')){
+        def version = getMavenProjectVersionParse()
+        println version
+        return version
+    } else {
+        def version = getGradleProjectVersionParse()
+        println version
+        return version
+    }
 }
 
 public String getMavenProjectVersionProcess(String exe){
-    def mavenExe = 'mvn'
-    if(exe) {
-        mavenExe = exe
+    try {
+        def mvnHome = tool "maven-3"
+        def mavenExe = "${mvnHome}/bin/mvn"
+        if(exe) {
+            mavenExe = exe
+        }
+        def version = sh(script: "${mavenExe} help:evaluate -Dexpression=project.version | grep -v '\\['", returnStdout: true)
+        return version
+    } catch (Exception e){
+        println "Failed to run the mvn command to get the Project version ${e.getMessage()}"
     }
-    def mavenProcess = "${mavenExe} help:evaluate -Dexpression=project.version | grep -v '\\['".execute()
-    mavenProcess.waitFor()
-    return mavenProcess.getText()
+    return null
 }
 
 public String getMavenProjectVersionParse(){
-    def fileText =  new File('pom.xml').text
+    def fileText = readFile file: "${WORKSPACE}/pom.xml"
     def project = new XmlSlurper().parseText(fileText)
     return project.version.text()
 }
 
-public String getGradleProjectVersion(String exe){
-    def gradleVersion = ''
-    try {
-        def gradleExe = './gradlew'
-        if(exe) {
+
+public String getGradleProjectVersionProcess(String exe){
+   try {
+       def gradleExe = './gradlew'
+       if(exe) {
             gradleExe = exe
-        }
-        def gradleProcess = "${gradleExe} properties -q | grep 'version:'".execute().waitFor()
-        gradleVersion = gradleProcess.getText()
-        gradleVersion = gradleVersion.substring(gradleVersion.indexOf(':') + 1).trim()
+       }
+       def version = sh(script: "${gradleExe} properties -q | grep 'version:'", returnStdout: true)
+       return version.substring(version.indexOf(':') + 1).trim()
     } catch (Exception e) {
         println "Failed to run the gradle command to get the Project version ${e.getMessage()}"
     }
-    if (!gradleVersion){
-        def versionLine = ''
-        new File('build.gradle').eachLine { line ->
-            def trimmedLine = line.trim()
-            if (!versionLine && trimmedLine.startsWith('version')) {
-                versionLine = trimmedLine;
-            }
-        }
-        gradleVersion = versionLine.substring(versionLine.indexOf('=') + 1).trim()
-    }
-    return gradleVersion
+    return null
 }
 
-public String getGradleProjectVersionProcess(String exe){
-   def gradleExe = './gradlew'
-   if(exe) {
-        gradleExe = exe
-   }
-   def gradleProcess = "${gradleExe} properties -q | grep 'version:'".execute().waitFor()
-   gradleVersion = gradleProcess.getText()
-   return gradleVersion.substring(gradleVersion.indexOf(':') + 1).trim()
-}
 
 public String getGradleProjectVersionParse(){
    def versionLine = ''
-   new File('build.gradle').eachLine { line ->
+   def fileText = readFile file: "${WORKSPACE}/build.gradle"
+   fileText.split('\n').each { line ->
        def trimmedLine = line.trim()
        if (!versionLine && trimmedLine.startsWith('version')) {
            versionLine = trimmedLine;
--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/groovy
 
+import com.synopsys.integration.ProjectUtils
+
 def call(String stageName = 'Setup', Closure body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
@@ -8,70 +10,7 @@ def call(String stageName = 'Setup', Closure body) {
 
     def tool = config.tool
     def exe = config.exe
-
-    if(tool.equals('maven')){
-        def version = getMavenProjectVersionProcess(exe)
-        if (!version){
-            version = getMavenProjectVersionParse()
-        }
-        println version
-        return version
-    } else {
-        def version = getGradleProjectVersionProcess(exe)
-        if (!version){
-            version = getGradleProjectVersionParse()
-        }
-        println version
-        return version
-    }
-}
-
-
-public String getMavenProjectVersionProcess(String exe){
-    try {
-        def mvnHome = tool "maven-3"
-        def mavenExe = "${mvnHome}/bin/mvn"
-        if(exe) {
-            mavenExe = exe
-        }
-        def version = sh(script: "${mavenExe} help:evaluate -Dexpression=project.version | grep -v '\\['", returnStdout: true)
-        return version
-    } catch (Exception e){
-        println "Failed to run the mvn command to get the Project version ${e.getMessage()}"
-    }
-    return null
-}
-
-public String getMavenProjectVersionParse(){
-    def fileText = readFile file: "${WORKSPACE}/pom.xml"
-    def project = new XmlSlurper().parseText(fileText)
-    return project.version.text()
-}
-
-
-public String getGradleProjectVersionProcess(String exe){
-   try {
-       def gradleExe = './gradlew'
-       if(exe) {
-            gradleExe = exe
-       }
-       def version = sh(script: "${gradleExe} properties -q | grep 'version:'", returnStdout: true)
-       return version.substring(version.indexOf(':') + 1).trim()
-    } catch (Exception e) {
-        println "Failed to run the gradle command to get the Project version ${e.getMessage()}"
-    }
-    return null
-}
-
-
-public String getGradleProjectVersionParse(){
-   def versionLine = ''
-   def fileText = readFile file: "${WORKSPACE}/build.gradle"
-   fileText.split('\n').each { line ->
-       def trimmedLine = line.trim()
-       if (!versionLine && trimmedLine.startsWith('version')) {
-           versionLine = trimmedLine;
-       }
-   }
-   return versionLine.substring(versionLine.indexOf('=') + 1).trim()
+    ProjectUtils projectUtils = new ProjectUtils()
+    def version = projectUtils.getProjectVersion(tool, exe)
+    println version
 }
