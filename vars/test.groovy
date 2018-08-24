90cc86843a2af345ba60b1c84f43a9d66e1e2658
jrichard <jrichard@synopsys.com>
2018-08-24 11:36:03 -0400

    Updating error handling

--- vars/test.groovy
+++ vars/test.groovy
@@ -10,23 +10,36 @@ def call(String stageName = 'Setup', Closure body) {
     def exe = config.exe
 
     if(tool.equals('maven')){
-        sh "echo ${getMavenProjectVersionProcess(exe)}"
-        sh "echo ${getMavenProjectVersionParse()}"
+        def version = getMavenProjectVersionProcess(exe)
+        if (!version){
+            version = getMavenProjectVersionParse()
+        }
+        println version
+        return version
     } else {
-        sh "echo ${getGradleProjectVersionProcess(exe)}"
-        sh "echo ${getGradleProjectVersionParse()}"
+        def version = getGradleProjectVersionProcess(exe)
+        if (!version){
+            version = getGradleProjectVersionParse()
+        }
+        println version
+        return version
     }
 }
 
 
 public String getMavenProjectVersionProcess(String exe){
-    def mvnHome = tool "maven-3"
-    def mavenExe = "${mvnHome}/bin/mvn"
-    if(exe) {
-        mavenExe = exe
+    try {
+        def mvnHome = tool "maven-3"
+        def mavenExe = "${mvnHome}/bin/mvn"
+        if(exe) {
+            mavenExe = exe
+        }
+        def version = sh(script: "${mavenExe} help:evaluate -Dexpression=project.version | grep -v '\\['", returnStdout: true)
+        return version
+    } catch (Exception e){
+        println "Failed to run the mvn command to get the Project version ${e.getMessage()}"
     }
-    def version = sh(script: "${mavenExe} help:evaluate -Dexpression=project.version | grep -v '\\['", returnStdout: true)
-    return version
+    return null
 }
 
 public String getMavenProjectVersionParse(){
@@ -37,13 +50,19 @@ public String getMavenProjectVersionParse(){
 
 
 public String getGradleProjectVersionProcess(String exe){
-   def gradleExe = './gradlew'
-   if(exe) {
-        gradleExe = exe
-   }
-   def version = sh(script: "${gradleExe} properties -q | grep 'version:'", returnStdout: true)
-   return version.substring(version.indexOf(':') + 1).trim()
+   try {
+       def gradleExe = './gradlew'
+       if(exe) {
+            gradleExe = exe
+       }
+       def version = sh(script: "${gradleExe} properties -q | grep 'version:'", returnStdout: true)
+       return version.substring(version.indexOf(':') + 1).trim()
+    } catch (Exception e) {
+        println "Failed to run the gradle command to get the Project version ${e.getMessage()}"
+    }
+    return null
 }
+
 
 public String getGradleProjectVersionParse(){
    def versionLine = ''
