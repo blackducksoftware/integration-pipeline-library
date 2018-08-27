86997ea69a0af49bf7293a82761e498031bb087e
jrichard <jrichard@synopsys.com>
2018-08-27 09:22:02 -0400

    Updating the pipeline to skip detect if the Hub is not reachable

--- vars/detectStage.groovy
+++ vars/detectStage.groovy
@@ -7,7 +7,7 @@ def call(String stageName = 'Run Detect', Closure body) {
     body()
 
     def detectURL = config.get('detectURL', "${HUB_DETECT_URL}")
-    def detectCommand = config.get('detectCommand', '')
+    def detectCommand = config.detectCommand + " --detect.disable.without.hub=true"
 
     def commandLines = []
     commandLines.add("#!/bin/bash")
