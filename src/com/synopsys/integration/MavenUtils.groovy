369c2be88cec931c6fb50783a5b125baaabe74e8
jrichard <jrichard@synopsys.com>
2018-08-28 11:39:49 -0400

    Updating split return type

--- src/com/synopsys/integration/MavenUtils.groovy
+++ src/com/synopsys/integration/MavenUtils.groovy
@@ -57,7 +57,7 @@ public class MavenUtils implements ToolUtils, Serializable {
         script.sh "${mavenExe} dependency:tree -DoutputFile=${script.env.WORKSPACE}/dependency.txt"
         def fileText = script.readFile file: "${script.env.WORKSPACE}/dependency.txt"
         println fileText
-        def splitLines = fileText.split('\n')
+        List<String> splitLines = fileText.split('\n')
         // need to remove the first line, since that is the GAV of the current project
         splitLines.remove(0)
         fileText = splitLines.join('\n')
