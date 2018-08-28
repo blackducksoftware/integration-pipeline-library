9b39f488bdd3c805363da0ff519df57729d32d44
jrichard <jrichard@synopsys.com>
2018-08-28 10:22:15 -0400

    Adding stages

--- vars/postReleaseStage.groovy
+++ vars/postReleaseStage.groovy
@@ -13,13 +13,16 @@ def call(String stageName = 'Post-Release Stage', Closure body) {
     def exe = config.exe
     def branch = config.get('branch', "${BRANCH}")
 
-    ProjectUtils projectUtils = new ProjectUtils(this)
-    if (runRelease) {
-        println "Using the next snapshot post release"
-        def newMavenVersion = projectUtils.increaseSemver('maven', exe)
-        def newGradleVersion = projectUtils.increaseSemver('gradle', exe)
-        sh "git commit -a -m \"Using the next snapshot post release ${newMavenVersion}  ${newGradleVersion}\""
-        sh "git push origin ${branch}"
+    stage(stageName) {
+        ProjectUtils projectUtils = new ProjectUtils(this)
+        if (runRelease) {
+            println "Using the next snapshot post release"
+            def newMavenVersion = projectUtils.increaseSemver('maven', exe)
+            def newGradleVersion = projectUtils.increaseSemver('gradle', exe)
+            sh "git commit -a -m \"Using the next snapshot post release ${newMavenVersion}  ${newGradleVersion}\""
+            sh "git push origin ${branch}"
+        } else {
+            println "Skipping the Post-Release because this was not a release build"
+        }
     }
-
 }
--- vars/preReleaseStage.groovy
+++ vars/preReleaseStage.groovy
@@ -14,29 +14,32 @@ def call(String stageName = 'Pre-Release Stage', Closure body) {
 
     def branch = config.get('branch', "${BRANCH}")
 
-    ProjectUtils projectUtils = new ProjectUtils(this)
-    if (runRelease) {
-        def hasMavenSnapshotDependencies = projectUtils.checkForSnapshotDependencies('maven', exe)
-        def hasGradleSnapshotDependencies = projectUtils.checkForSnapshotDependencies('gradle', exe)
-        if (hasMavenSnapshotDependencies || hasGradleSnapshotDependencies) {
-            def errorMessage = ''
-            if (hasMavenSnapshotDependencies) {
-                errorMessage += 'Failing release build because of Maven SNAPSHOT dependencies'
+    stage(stageName) {
+        ProjectUtils projectUtils = new ProjectUtils(this)
+        if (runRelease) {
+            def hasMavenSnapshotDependencies = projectUtils.checkForSnapshotDependencies('maven', exe)
+            def hasGradleSnapshotDependencies = projectUtils.checkForSnapshotDependencies('gradle', exe)
+            if (hasMavenSnapshotDependencies || hasGradleSnapshotDependencies) {
+                def errorMessage = ''
+                if (hasMavenSnapshotDependencies) {
+                    errorMessage += 'Failing release build because of Maven SNAPSHOT dependencies'
+                }
+                if (hasGradleSnapshotDependencies) {
+                    errorMessage += 'Failing release build because of Gradle SNAPSHOT dependencies'
+                }
+                throw new Exception(errorMessage)
             }
-            if (hasGradleSnapshotDependencies) {
-                errorMessage += 'Failing release build because of Gradle SNAPSHOT dependencies'
-            }
-            throw new Exception(errorMessage)
-        }
-        def version = projectUtils.getProjectVersion('maven', exe)
+            def version = projectUtils.getProjectVersion('maven', exe)
 
-        if (version.contains('-SNAPSHOT')) {
-            println "Removing SNAPSHOT from the Project Version"
-            def newMavenVersion = projectUtils.removeSnapshotFromProjectVersion('maven', exe)
-            def newGradleVersion = projectUtils.removeSnapshotFromProjectVersion('gradle', exe)
-            sh "git commit -a -m \"Release ${newMavenVersion}  ${newGradleVersion}\""
-            sh "git push origin ${branch}"
+            if (version.contains('-SNAPSHOT')) {
+                println "Removing SNAPSHOT from the Project Version"
+                def newMavenVersion = projectUtils.removeSnapshotFromProjectVersion('maven', exe)
+                def newGradleVersion = projectUtils.removeSnapshotFromProjectVersion('gradle', exe)
+                sh "git commit -a -m \"Release ${newMavenVersion}  ${newGradleVersion}\""
+                sh "git push origin ${branch}"
+            }
+        } else {
+            println "Skipping the Post-Release because this was not a release build"
         }
     }
-
 }
