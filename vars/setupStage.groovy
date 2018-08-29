bbde49425a33e6b13d5251c999985903aa672de8
jrichard <jrichard@synopsys.com>
2018-08-29 15:14:57 -0400

    Updating log message

--- vars/setupStage.groovy
+++ vars/setupStage.groovy
@@ -10,8 +10,7 @@ def call(String stageName = 'Setup', Boolean cleanupWorkspace = true, Closure bo
                     sh "rm -rf *"
                     sh "rm -rf .*"
                 } catch (Exception e) {
-                    println "Ignore this error if the only problem was something like 'refusing to remove ...'"
-                    println "There was a problem cleaning the workspace ${e.getMessage()}"
+                    println "Ignore this error if the only problem was something like 'refusing to remove ...'\nThere was a problem cleaning the workspace ${e.getMessage()}"
                 }
             } else {
                 println "Skipping the workspace cleanup since the ${WORKSPACE} does not exist"
