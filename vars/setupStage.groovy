11368cbdfc132f83cab5466ff18cd556e442012d
jrichard <jrichard@synopsys.com>
2018-08-29 11:30:22 -0400

    Updating the setup stage

--- vars/setupStage.groovy
+++ vars/setupStage.groovy
@@ -2,9 +2,15 @@
 
 def call(String stageName = 'Setup', Boolean cleanupWorkspace = true, Closure body) {
     stage(stageName) {
-        if (cleanupWorkspace && new File("${WORKSPACE}").exists()) {
-            sh "rm -rf ${WORKSPACE}"
-            sh "mkdir ${WORKSPACE}"
+        if (cleanupWorkspace) {
+            File workspace = new File("${WORKSPACE}")
+            if (new File("${WORKSPACE}").exists()) {
+                println "Cleaning the workspace"
+                sh "rm -rf ${WORKSPACE}"
+                sh "mkdir ${WORKSPACE}"
+            } else {
+                println "Skipping the workspace cleanup since the ${WORKSPACE} does not exist"
+            }
         }
         body()
     }
