ea524254fc6d4782d7d12773ec1745f00aa78950
jrichard <jrichard@synopsys.com>
2018-08-30 13:21:02 -0400

    Updating integration node label

--- vars/integrationNode.groovy
+++ vars/integrationNode.groovy
@@ -1,7 +1,11 @@
 #!/usr/bin/groovy
 
-def call(String nodeLabel = 'integrations', Closure body) {
-    node(nodeLabel) {
+def call(String nodeLabel, Closure body) {
+    String label = nodeLabel
+    if (null == nodeLabel || nodeLabel.trim().length() == 0) {
+        label = 'integrations'
+    }
+    node(label) {
         body()
     }
 }
