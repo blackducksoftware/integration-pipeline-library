47e4eaa7813c851b3b5b70d4eb6775a38a5a37be
Steve Billings <sbillings@blackducksoftware.com>
2018-09-27 15:47:03 -0400

    INTCMN-301: cleanup in getReleaser.sh

--- scripts/releaseLibs/getReleaser.sh
+++ scripts/releaseLibs/getReleaser.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 #
-# Run this bash script in an empty directory to pull down the groovy script files for releasing libraries (the "Releaser script"). Then:
-#
-#	- To change the library list (you shouldn't need to), edit Libraries.groovy
-#	- For instructions on how to run the script, run: groovy Releaser.groovy
-#
-# The Releaser script requires that groovy be on your path.
+# Run this bash script in an empty directory to pull down the groovy script files for releasing libraries (the "Releaser script").
 #
 curl -O https://raw.githubusercontent.com/blackducksoftware/integration-pipeline-library/master/scripts/releaseLibs/ToolRunner.groovy
 curl -O https://raw.githubusercontent.com/blackducksoftware/integration-pipeline-library/master/scripts/releaseLibs/Releaser.groovy
