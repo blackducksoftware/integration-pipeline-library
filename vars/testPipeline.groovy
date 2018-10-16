8822ca41e839317d42a5732d617c884cc316586b
jrichard <jrichard@synopsys.com>
2018-10-16 13:39:52 -0400

    Simplifying the testPipeline

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -1,5 +1,4 @@
 #!/usr/bin/groovy
-import hudson.model.ParameterDefinition
 
 def call(Closure body) {
     def config = [:]
@@ -14,103 +13,34 @@ def call(Closure body) {
     String gitUrlVar = config.gitUrl
     String gitRelativeTargetDirVar = config.gitRelativeTargetDir
 
-    Closure preBuildBody = config.preBuild
     String gradleCommandVar = config.buildCommand
-    Closure postBuildBody = config.postBuild
 
-    String detectCommandVar = config.detectCommand
 
-    boolean runGitHubReleaseVar = config.get('runGitHubRelease', true)
-    String gradleExeVar = config.gradleExe
-    String releaseVersionVar = config.releaseVersion
-    String ownerVar = config.owner
-    String artifactFileVar = config.artifactFile
-    String artifactPatternVar = config.artifactPattern
-    String artifactDirectoryVar = config.artifactDirectory
-    String projectVar = config.project
-    String releaseDescriptionVar = config.releaseDescription
-
-    boolean runArchiveVar = config.get('runArchive', true)
-    String archivePatternVar = config.archivePattern
-
-    boolean runJunitVar = config.get('runJunit', true)
-    String junitXmlPatternVar = config.junitXmlPattern
-
-    boolean runJacocoVar = config.get('runJacoco', true)
-
-    List<ParameterDefinition> params = []
+    def params = []
     params.add(booleanParam(defaultValue: false, description: 'If you want to release the project, set this to true', name: 'RUN_RELEASE'))
     params.add(string(defaultValue: 'Auto Release', description: 'The release note that you want the Auto Release tool to display.', name: 'COMMIT_MESSAGE', trim: true))
     params.add(string(defaultValue: 'master', description: 'The branch you want to build', name: 'BRANCH', trim: true))
     params.addAll(additionalParameters)
 
-    pipeline {
-        agent none
-        parameters {
-            params
-        }
-        stages {
-            stage('Example') {
-                steps {
-                    script {
-                        integrationNode(nodeNameVar) {
-                            emailWrapper(emailListVar) {
-                                setupStage {
-                                    setJdk {}
-                                }
-                                def directoryToRunIn = gitStage {
-                                    url = gitUrlVar
-                                    gitRelativeTargetDir = gitRelativeTargetDirVar
-                                }
-                                dir(directoryToRunIn) {
-                                    if (null != preBuildBody) {
-                                        stage('Pre Build') {
-                                            preBuildBody()
-                                        }
-                                    }
-                                    gradleStage {
-                                        buildCommand = gradleCommandVar
-                                    }
-                                    if (null != postBuildBody) {
-                                        stage('Post Build') {
-                                            postBuildBody()
-                                        }
-                                    }
-                                    if (runGitHubReleaseVar) {
-                                        newGarStage {
-                                            buildTool = 'gradle'
-                                            exe = gradleExeVar
-                                            releaseVersion = releaseVersionVar
-                                            owner = ownerVar
-                                            artifactFile = artifactFileVar
-                                            artifactPattern = artifactPatternVar
-                                            artifactDirectory = artifactDirectoryVar
-                                            project = projectVar
-                                            releaseDescription = releaseDescriptionVar
-                                        }
-                                    }
-                                    if (runArchiveVar) {
-                                        archiveStage {
-                                            patterns = archivePatternVar
-                                        }
-                                    }
-                                    if (runJunitVar) {
-                                        junitStage {
-                                            xmlPattern = junitXmlPatternVar
-                                        }
-                                    }
-                                    if (runJacocoVar) {
-                                        jacocoStage {}
-                                    }
-                                    detectStage {
-                                        detectCommand = detectCommandVar
-                                    }
-                                }
-                            }
-                        }
-                    }
+    properties([parameters(params),
+                disableConcurrentBuilds()])
+
+    integrationNode(nodeNameVar) {
+        emailWrapper(emailListVar) {
+            setupStage {
+                setJdk {}
+            }
+            def directoryToRunIn = gitStage {
+                url = gitUrlVar
+                gitRelativeTargetDir = gitRelativeTargetDirVar
+            }
+            dir(directoryToRunIn) {
+                gradleStage {
+                    buildCommand = gradleCommandVar
                 }
             }
         }
     }
+
+
 }
