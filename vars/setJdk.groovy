1f6ffc034969ba33e0b2bae4ada447806f6200c7
jrichard <jrichard@synopsys.com>
2018-12-18 17:05:21 -0500

    Adding logs to debug issues

--- vars/setJdk.groovy
+++ vars/setJdk.groovy
@@ -8,8 +8,12 @@ def call(Closure body) {
 
     String jdk = config.jdk ?: 'jdk8'
 
+    println("Setting jdk = ${jdk}")
+
     String jdkTool = tool "${jdk}"
     env.JAVA_HOME = "${jdkTool}"
     env.PATH = "${JAVA_HOME}/bin:${PATH}"
 
+    println("JAVA_HOME = ${env.JAVA_HOME}")
+    println("PATH = ${env.PATH}")
 }
