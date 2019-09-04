86a40056b13aa0040842d0a395c64e756e86ef25
jrichard <jrichard@synopsys.com>
2019-09-04 18:20:08 -0400

    Updating the emailext usage

--- src/com/synopsys/integration/pipeline/jenkins/ScriptWrapper.groovy
+++ src/com/synopsys/integration/pipeline/jenkins/ScriptWrapper.groovy
@@ -30,7 +30,7 @@ class ScriptWrapper {
     }
 
     public Object emailext(String content, String subjectLine, String recipientList) {
-        return script.emailext(body: "content", subject: "subjectLine", to: "recipientList")
+        return script.emailext(content, subjectLine, recipientList)
     }
 
     public String tool(String toolName) {
