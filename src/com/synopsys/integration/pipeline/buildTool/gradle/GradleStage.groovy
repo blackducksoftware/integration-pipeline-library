cb6064f9243802877fc5efe12f2e98b5eb39bcfe
jrichard <jrichard@synopsys.com>
2019-09-05 16:02:06 -0400

    Updating constructors

--- src/com/synopsys/integration/pipeline/buildTool/gradle/GradleStage.groovy
+++ src/com/synopsys/integration/pipeline/buildTool/gradle/GradleStage.groovy
@@ -19,7 +19,7 @@ class GradleStage extends Stage {
     }
 
     public GradleStage(ScriptWrapper scriptWrapper, String stageName, String gradleExe, String gradleOptions) {
-        super(stageName)
+        super(stageName, scriptWrapper)
         this.scriptWrapper = scriptWrapper
 
         if (null != gradleExe && gradleExe.trim().length() > 0) {
--- src/com/synopsys/integration/pipeline/buildTool/maven/MavenStage.groovy
+++ src/com/synopsys/integration/pipeline/buildTool/maven/MavenStage.groovy
@@ -16,7 +16,7 @@ class MavenStage extends Stage {
     }
 
     public MavenStage(ScriptWrapper scriptWrapper, String stageName, String mavenToolName, String mavenOptions) {
-        super(stageName)
+        super(stageName, scriptWrapper)
         this.scriptWrapper = scriptWrapper
 
         if (null != mavenToolName && mavenToolName.trim().length() > 0) {
