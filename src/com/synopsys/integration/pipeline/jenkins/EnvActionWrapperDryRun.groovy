22a64bdf55794af362c44d23f236bb9fafa0e17b
jrichard <jrichard@synopsys.com>
2019-09-17 15:47:24 -0400

    debugging issue

--- src/com/synopsys/integration/pipeline/jenkins/EnvActionWrapperDryRun.groovy
+++ src/com/synopsys/integration/pipeline/jenkins/EnvActionWrapperDryRun.groovy
@@ -4,16 +4,19 @@ package com.synopsys.integration.pipeline.jenkins
 import org.jenkinsci.plugins.workflow.cps.EnvActionImpl
 
 class EnvActionWrapperDryRun extends EnvActionWrapperImpl {
-    public final DryRunPipelineBuilder dryRunPipelineBuilder
+    public final DryRunPipelineBuilder dryRunBuilder
 
     EnvActionWrapperDryRun(EnvActionImpl envAction, DryRunPipelineBuilder dryRunPipelineBuilder) {
         super(envAction)
-        this.dryRunPipelineBuilder = dryRunPipelineBuilder
+        this.dryRunBuilder = dryRunPipelineBuilder
     }
 
     @Override
     public void setProperty(String propertyName, Object newValue) {
-        this.dryRunPipelineBuilder.addPipelineLine("setProperty(propertName:${propertyName} value:${newValue})")
+        getDryRunBuilder().addPipelineLine("setProperty(propertName:${propertyName} value:${newValue})")
     }
 
+    public DryRunPipelineBuilder getDryRunBuilder() {
+        return dryRunBuilder
+    }
 }
