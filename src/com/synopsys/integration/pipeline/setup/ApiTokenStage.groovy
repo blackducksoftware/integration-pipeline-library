37a43925d305ff9969d0e3c22b0faa9e88f85d9b
Eric Kerwin <ekerwin@synopsys.com>
2021-03-19 12:18:11 -0400

    convert to groovy

--- src/com/synopsys/integration/pipeline/setup/ApiTokenStage.groovy
+++ src/com/synopsys/integration/pipeline/setup/ApiTokenStage.groovy
@@ -5,33 +5,32 @@ import com.synopsys.integration.pipeline.jenkins.PipelineConfiguration
 import com.synopsys.integration.pipeline.model.Stage
 
 class ApiTokenStage extends Stage {
-    public static final String REQUEST_FORMAT = "%s/puretoken?vm=%s&name=%s&username=%s"
-
     ApiTokenStage(PipelineConfiguration pipelineConfiguration, String name) {
         super(pipelineConfiguration, name)
     }
 
     @Override
     void stageExecution() throws PipelineException, Exception {
-        String apiTokensUrl = getPropertyValue("BLACKDUCK_API_TOKENS_URL")
-        String blackDuckUrl = getPropertyValue("BLACKDUCK_URL")
-        String blackDuckApiTokenName = getPropertyValue("BLACKDUCK_API_TOKEN_NAME")
-        String blackDuckApiTokenUsername = getPropertyValue("BLACKDUCK_API_TOKEN_USERNAME")
+        String apiTokensUrl = getPropertyValue('BLACKDUCK_API_TOKENS_URL')
+        String blackDuckUrl = getPropertyValue('BLACKDUCK_URL')
+        String blackDuckApiTokenName = getPropertyValue('BLACKDUCK_API_TOKEN_NAME')
+        String blackDuckApiTokenUsername = getPropertyValue('BLACKDUCK_API_TOKEN_USERNAME')
 
         if (blackDuckUrl) {
-            String url = String.format(REQUEST_FORMAT, apiTokensUrl, blackDuckUrl, blackDuckApiTokenName, blackDuckApiTokenUsername)
+            String url = "${apiTokensUrl}/puretoken?vm=${blackDuckUrl}&name=${blackDuckApiTokenName}&username=${blackDuckApiTokenUsername}"
             URL apiTokenURL = new URL(url)
             HttpURLConnection httpURLConnection = (HttpURLConnection) apiTokenURL.openConnection()
             httpURLConnection.connect()
             new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream())).withCloseable {
                 String blackDuckApiToken = it.readLine()
-                pipelineConfiguration.scriptWrapper.setJenkinsProperty("BLACKDUCK_API_TOKEN", blackDuckApiToken)
+                pipelineConfiguration.scriptWrapper.setJenkinsProperty('BLACKDUCK_API_TOKEN', blackDuckApiToken)
             }
         }
     }
 
     private String getPropertyValue(String propertyName) {
-        return pipelineConfiguration.scriptWrapper.getJenkinsProperty(propertyName)
+        String value = pipelineConfiguration.scriptWrapper.getJenkinsProperty(propertyName)
+        return value ? value : ''
     }
 
 }
