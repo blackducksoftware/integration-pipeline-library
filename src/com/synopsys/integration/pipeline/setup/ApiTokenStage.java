ed3a8258636043851c98810f1336de584b68c847
Eric Kerwin <ekerwin@synopsys.com>
2021-03-19 11:45:01 -0400

    removing constructor call

--- src/com/synopsys/integration/pipeline/setup/ApiTokenStage.java
+++ src/com/synopsys/integration/pipeline/setup/ApiTokenStage.java
@@ -5,8 +5,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.synopsys.integration.pipeline.exception.PipelineException;
 import com.synopsys.integration.pipeline.jenkins.PipelineConfiguration;
 import com.synopsys.integration.pipeline.model.Stage;
@@ -20,21 +18,21 @@ public class ApiTokenStage extends Stage {
 
     @Override
     public void stageExecution() throws PipelineException, Exception {
-        String apiTokensUrl = getPropertyValue("BLACKDUCK_API_TOKENS_URL");
-        String blackDuckUrl = getPropertyValue("BLACKDUCK_URL");
-        String blackDuckApiTokenName = getPropertyValue("BLACKDUCK_API_TOKEN_NAME");
-        String blackDuckApiTokenUsername = getPropertyValue("BLACKDUCK_API_TOKEN_USERNAME");
-
-        if (StringUtils.isNotBlank(blackDuckUrl)) {
-            String url = String.format(REQUEST_FORMAT, apiTokensUrl, blackDuckUrl, blackDuckApiTokenName, blackDuckApiTokenUsername);
-            URL apiTokenURL = new URL(url);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) apiTokenURL.openConnection();
-            httpURLConnection.connect();
-            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()))) {
-                String blackDuckApiToken = bufferedReader.readLine();
-                pipelineConfiguration.scriptWrapper.setJenkinsProperty("BLACKDUCK_API_TOKEN", blackDuckApiToken);
-            }
-        }
+//        String apiTokensUrl = getPropertyValue("BLACKDUCK_API_TOKENS_URL");
+//        String blackDuckUrl = getPropertyValue("BLACKDUCK_URL");
+//        String blackDuckApiTokenName = getPropertyValue("BLACKDUCK_API_TOKEN_NAME");
+//        String blackDuckApiTokenUsername = getPropertyValue("BLACKDUCK_API_TOKEN_USERNAME");
+//
+//        if (null != blackDuckUrl && !"".equals(blackDuckUrl)) {
+//            String url = String.format(REQUEST_FORMAT, apiTokensUrl, blackDuckUrl, blackDuckApiTokenName, blackDuckApiTokenUsername);
+//            URL apiTokenURL = new URL(url);
+//            HttpURLConnection httpURLConnection = (HttpURLConnection) apiTokenURL.openConnection();
+//            httpURLConnection.connect();
+//            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()))) {
+//                String blackDuckApiToken = bufferedReader.readLine();
+//                pipelineConfiguration.scriptWrapper.setJenkinsProperty("BLACKDUCK_API_TOKEN", blackDuckApiToken);
+//            }
+//        }
     }
 
     private String getPropertyValue(String propertyName) {
