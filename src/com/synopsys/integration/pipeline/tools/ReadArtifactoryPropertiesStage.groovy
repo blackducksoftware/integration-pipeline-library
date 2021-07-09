ac19b909a9e7cb6c100c16ae4f89634a7838b91d
Eric Kerwin <ekerwin@synopsys.com>
2021-07-08 20:55:15 -0400

    removing cps

--- src/com/synopsys/integration/pipeline/tools/ReadArtifactoryPropertiesStage.groovy
+++ src/com/synopsys/integration/pipeline/tools/ReadArtifactoryPropertiesStage.groovy
@@ -8,8 +8,26 @@ class ReadArtifactoryPropertiesStage extends Stage {
     public static final String PUBLIC_ARTIFACTORY = 'https://sig-repo.synopsys.com/artifactory'
     public static final String INTERNAL_ARTIFACTORY = 'https://artifactory.internal.synopsys.com/artifactory'
 
+    public static final String NUGET_REPO = 'bds-integrations-nuget-release'
+    public static final String MAVEN_REPO = 'bds-integrations-release'
+
+    public static final String DETECT_REPOPATH = 'com/synopsys/integration/synopsys-detect'
+    public static final String DOCKER_INSPECTOR_REPOPATH = 'com/synopsys/integration/blackduck-docker-inspector'
+
+    private final List<ArtifactoryProduct> artifactoryProducts = new LinkedList<>()
+
     ReadArtifactoryPropertiesStage(PipelineConfiguration pipelineConfiguration, String name) {
         super(pipelineConfiguration, name)
+        artifactoryProducts.add(create(NUGET_REPO, 'BlackduckNugetInspector', 'NUGET_INSPECTOR'))
+        artifactoryProducts.add(create(NUGET_REPO, 'IntegrationNugetInspector', 'NUGET_INSPECTOR'))
+        artifactoryProducts.add(create(NUGET_REPO, 'NugetDotnet3Inspector', 'NUGET_DOTNET3_INSPECTOR'))
+        artifactoryProducts.add(create(NUGET_REPO, 'NugetDotnet5Inspector', 'NUGET_DOTNET5_INSPECTOR'))
+
+        artifactoryProducts.add(create(MAVEN_REPO, DETECT_REPOPATH, 'DETECT'))
+        artifactoryProducts.add(create(MAVEN_REPO, DETECT_REPOPATH, 'DETECT_FONT_BUNDLE'))
+
+        artifactoryProducts.add(create(MAVEN_REPO, DOCKER_INSPECTOR_REPOPATH, 'DOCKER_INSPECTOR_AIR_GAP'))
+        artifactoryProducts.add(create(MAVEN_REPO, DOCKER_INSPECTOR_REPOPATH, 'DOCKER_INSPECTOR'))
     }
 
     @NonCPS
@@ -19,6 +37,10 @@ class ReadArtifactoryPropertiesStage extends Stage {
             String itemPath = artifactoryProduct.getItemPathToCheck()
             pipelineConfiguration.getLogger().info(String.format("Properties for: %s/%s", repoKey, itemPath))
         }
+    }
+
+    private ArtifactoryProduct create(String repo, String repoPathToCheck, String propertyPrefix) {
+        return new ArtifactoryProduct(repo, repoPathToCheck, propertyPrefix)
     }
 
     @Override
