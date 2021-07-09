acf2b25e3131bdbb5e804019d44a5027e94741a9
Eric Kerwin <ekerwin@synopsys.com>
2021-07-08 20:52:41 -0400

    removing cps

--- src/com/synopsys/integration/pipeline/tools/ReadArtifactoryPropertiesStage.groovy
+++ src/com/synopsys/integration/pipeline/tools/ReadArtifactoryPropertiesStage.groovy
@@ -12,31 +12,34 @@ class ReadArtifactoryPropertiesStage extends Stage {
         super(pipelineConfiguration, name)
     }
 
-    @Override
     @NonCPS
-    void stageExecution() throws Exception {
+    private void doTheStuff() {
         for (ArtifactoryProduct artifactoryProduct : ArtifactoryProducts.artifactoryProducts) {
             String repoKey = artifactoryProduct.getRepoKey()
             String itemPath = artifactoryProduct.getItemPathToCheck()
             pipelineConfiguration.getLogger().info(String.format("Properties for: %s/%s", repoKey, itemPath))
-
-            //            HttpUrl propertiesUrl = new HttpUrl(String.format("%s/api/storage/%s/%s", PUBLIC_ARTIFACTORY, repoKey, itemPath))
-            //            URLConnection urlConnection = propertiesUrl.url().openConnection()
-            //            urlConnection.connect()
-            //            urlConnection.getInputStream().withCloseable { inputStream ->
-            //                String content = inputStream.text
-            //                ArtifactoryPropertiesResponse propertiesResponse = gson.fromJson(content, ArtifactoryPropertiesResponse.class)
-            //                Map<String, List<String>> properties = propertiesResponse.getProperties()
-            //                pipelineConfiguration.getLogger().info(String.format("Properties for: %s/%s", repoKey, itemPath))
-            //                for (String name : properties.keySet()) {
-            //                    if (name.startsWith(artifactoryProduct.getPropertyPrefix())) {
-            //                        String values = StringUtils.join(properties.get(name))
-            //                        String propertyOutput = String.format("%s: %s", name, values)
-            //                        pipelineConfiguration.getLogger().info(propertyOutput)
-            //                    }
-            //                }
-            //            }
         }
+    }
+
+    @Override
+    void stageExecution() throws Exception {
+        doTheStuff()
+        //            HttpUrl propertiesUrl = new HttpUrl(String.format("%s/api/storage/%s/%s", PUBLIC_ARTIFACTORY, repoKey, itemPath))
+        //            URLConnection urlConnection = propertiesUrl.url().openConnection()
+        //            urlConnection.connect()
+        //            urlConnection.getInputStream().withCloseable { inputStream ->
+        //                String content = inputStream.text
+        //                ArtifactoryPropertiesResponse propertiesResponse = gson.fromJson(content, ArtifactoryPropertiesResponse.class)
+        //                Map<String, List<String>> properties = propertiesResponse.getProperties()
+        //                pipelineConfiguration.getLogger().info(String.format("Properties for: %s/%s", repoKey, itemPath))
+        //                for (String name : properties.keySet()) {
+        //                    if (name.startsWith(artifactoryProduct.getPropertyPrefix())) {
+        //                        String values = StringUtils.join(properties.get(name))
+        //                        String propertyOutput = String.format("%s: %s", name, values)
+        //                        pipelineConfiguration.getLogger().info(propertyOutput)
+        //                    }
+        //                }
+        //            }
     }
 
 }
