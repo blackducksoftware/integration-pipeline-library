61067c955a3d19eaed1a16fdc00ed105f6f7933d
Dana Maxfield <danam@synopsys.com>
2021-11-18 12:13:54 -0500

    Try to get gh-pages publish working with https on new build server

--- vars/integrationNode.groovy
+++ vars/integrationNode.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/groovy
 
-def call(String nodeLabel = 'integrations', Closure body) {
+def call(String nodeLabel = 'integrations-bd', Closure body) {
     String label = nodeLabel
     if (null == nodeLabel || nodeLabel.trim().length() == 0) {
-        label = 'integrations'
+        label = 'integrations-bd'
     }
     node(label) {
         body()
