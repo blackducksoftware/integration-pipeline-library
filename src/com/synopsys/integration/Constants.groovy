459a3c8b3c74ee87396600a3624602b9e2c75ac5
Richard Otte <rotte@blackducksoftware.com>
2022-01-14 15:40:56 -0500

    ci: Updated central integrations distribution list

--- src/com/synopsys/integration/Constants.groovy
+++ src/com/synopsys/integration/Constants.groovy
@@ -1,6 +1,6 @@
 package com.synopsys.integration;
 
 public class Constants {
-    public static final String CENTRAL_INTEGRATIONS_TEAM_EMAIL = "suicidesquad@synopsys.com";
+    public static final String CENTRAL_INTEGRATIONS_TEAM_EMAIL = "centralintegrations@synopsys.com";
     public static final String INTEGRATIONS_TEAM_EMAIL = "justiceleague@synopsys.com";
 }
