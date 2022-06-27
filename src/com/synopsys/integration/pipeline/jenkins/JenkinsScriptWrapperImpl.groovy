105b10b8c87efc57b437b0a16db3117830fbe240
github848 <maaustin@synopsys.com>
2022-06-27 13:45:15 -0500

    off by one counting

--- src/com/synopsys/integration/pipeline/jenkins/JenkinsScriptWrapperImpl.groovy
+++ src/com/synopsys/integration/pipeline/jenkins/JenkinsScriptWrapperImpl.groovy
@@ -87,7 +87,7 @@ class JenkinsScriptWrapperImpl implements JenkinsScriptWrapper {
     @Override
     String executeCommandWithHttpStatusCheck(String command, String expectedHttpStatusCode) {
         // New line for http_code??
-        String newCommand = command.substring(21, command.length()-1)
+        String newCommand = command.substring(20, command.length()-1)
         String stdOut = executeCommand(newCommand)
 
         // Edit stdOut into 2 pieces: 1st is the json, 2nd is the http_status_code
