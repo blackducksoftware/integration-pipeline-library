267504e2b4b3cc140d07b8af59eab0fc27d961d5
github848 <maaustin@synopsys.com>
2022-07-07 09:14:34 -0500

    typo

--- src/com/synopsys/integration/pipeline/versioning/GithubAssetStage.groovy
+++ src/com/synopsys/integration/pipeline/versioning/GithubAssetStage.groovy
@@ -11,7 +11,7 @@ import java.text.SimpleDateFormat
 class GithubAssetStage extends Stage{
     public static String ASSET_FILE = 'assets.json'
     private String githubToken
-    private ArrayList<String> assetNames
+    private String[] assetNames
 
     GithubAssetStage(PipelineConfiguration pipelineConfiguration, String stageName, String[] assetNames) {
         super(pipelineConfiguration, stageName)
