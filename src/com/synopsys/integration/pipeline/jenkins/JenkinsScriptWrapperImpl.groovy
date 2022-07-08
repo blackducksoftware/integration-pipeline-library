d6a78d29610934eda65ce0a4aec008b88ee6250a
github848 <maaustin@synopsys.com>
2022-07-08 14:07:59 -0500

    added bracket

--- src/com/synopsys/integration/pipeline/jenkins/JenkinsScriptWrapperImpl.groovy
+++ src/com/synopsys/integration/pipeline/jenkins/JenkinsScriptWrapperImpl.groovy
@@ -89,7 +89,7 @@ class JenkinsScriptWrapperImpl implements JenkinsScriptWrapper {
         // adding the http code checker command and sending output into jsonResponseFileName file
         script.withCredentials([script.usernamePassword(credentialsId: githubCredentialsId, usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
             String gitPassword = pipelineConfiguration.getScriptWrapper().getJenkinsProperty('GIT_PASSWORD')
-
+        }
         String newCommand = command + "${gitPassword}\" -o ${jsonResponseFileName} -w %{http_code}"
 
         //taking the Http status code
