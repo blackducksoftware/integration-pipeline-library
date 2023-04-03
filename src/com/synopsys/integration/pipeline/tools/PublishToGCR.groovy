2ec06c8c15fcdcf92b733b473ed275ed55ac6574
Dana Maxfield <danam@synopsys.com>
2023-04-03 13:16:01 -0400

    Perform logout before pull

--- src/com/synopsys/integration/pipeline/tools/PublishToGCR.groovy
+++ src/com/synopsys/integration/pipeline/tools/PublishToGCR.groovy
@@ -45,6 +45,7 @@ class PublishToGCR extends Stage {
 
         // Loop all image names to get the sha
         for (imageName in dockerImageNames) {
+            jenkinsScriptWrapper.executeCommandWithException("docker logout")
             jenkinsScriptWrapper.executeCommandWithException("docker pull ${imageName}")
 
             // Get and check the sha
