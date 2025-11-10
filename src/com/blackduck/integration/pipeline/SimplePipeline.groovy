4bee7abfba86d21da0c0ea5e7503ac1f4cda3af5
Dana Maxfield <danam@blackduck.com>
2025-11-10 11:34:33 -0500

    IALERT-3910 - Add in some debug logging

--- src/com/blackduck/integration/pipeline/SimplePipeline.groovy
+++ src/com/blackduck/integration/pipeline/SimplePipeline.groovy
@@ -175,6 +175,7 @@ class SimplePipeline extends Pipeline {
         DockerImage dockerImage = new DockerImage(pipelineConfiguration, imageName, codeLocationNameAsImage)
         DetectStage detectDockerStage = addDetectPopStage(dockerImage.getBdProjectName(), detectCommand)
         detectDockerStage.setDockerImage(dockerImage)
+        throw new IllegalArgumentException("codeLocationNameAsImage is -->" + codeLocationNameAsImage)
         return detectDockerStage
     }
 
